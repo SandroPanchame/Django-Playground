0c8481ee8c586303b5ec3b04f36e146e01db4758
SandroPanchame <spanchame@gmail.com>
2024-04-24 18:34:27 -0500

    5-Attaching Files, finished

--- UltDjangoCourse/Part3/storefront3/playground/views.py
+++ UltDjangoCourse/Part3/storefront3/playground/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
 
 def say_hello(request):
     try:
         # send_mail('subject','message', 'info@moshbuy.com', ['bob@moshbuy.com'])
-        #
-        mail_admins('subject', 'message', html_message='message')
+        # mail for admins, the formating differs. html instead of plain text
+        # mail_admins('subject', 'message', html_message='message')
+        message = EmailMessage('subject', 'message','from@moshbuy.com', ['john@moshbuy.com'])
+        message.attach_file('playground/static/images/images.png')
+        message.send()
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
