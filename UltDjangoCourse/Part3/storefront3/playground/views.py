9ad11f6cc65bb1cca12615ed24e681b794f64a28
SandroPanchame <spanchame@gmail.com>
2024-04-24 18:49:27 -0500

    6-Sending Templated Emails, finished

--- UltDjangoCourse/Part3/storefront3/playground/views.py
+++ UltDjangoCourse/Part3/storefront3/playground/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from django.core.mail import send_mail, mail_admins, BadHeaderError, EmailMessage
+from templated_mail.mail import BaseEmailMessage
 
 def say_hello(request):
     try:
         # send_mail('subject','message', 'info@moshbuy.com', ['bob@moshbuy.com'])
         # mail for admins, the formating differs. html instead of plain text
         # mail_admins('subject', 'message', html_message='message')
-        message = EmailMessage('subject', 'message','from@moshbuy.com', ['john@moshbuy.com'])
-        message.attach_file('playground/static/images/images.png')
-        message.send()
+        # message = EmailMessage('subject', 'message','from@moshbuy.com', ['john@moshbuy.com'])
+        # message.attach_file('playground/static/images/images.png')
+        message = BaseEmailMessage(
+            template_name='emails/hello.html',
+            context={'name': 'Mosh'}
+        )
+        message.send(['john@moshbuy.com'])
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Mosh'})
