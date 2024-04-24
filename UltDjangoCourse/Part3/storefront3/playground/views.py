576dba3eca3e60269543d3a63e50503c6554fdad
SandroPanchame <spanchame@gmail.com>
2024-04-24 16:48:43 -0500

    4-Sending Emails, finished

--- UltDjangoCourse/Part3/storefront3/playground/views.py
+++ UltDjangoCourse/Part3/storefront3/playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail, mail_admins, BadHeaderError
 
 def say_hello(request):
+    try:
+        # send_mail('subject','message', 'info@moshbuy.com', ['bob@moshbuy.com'])
+        #
+        mail_admins('subject', 'message', html_message='message')
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
