e13ca722ff3a8d98b47ecabb4edc6826d7066488
SandroPanchame <spanchame@gmail.com>
2024-04-25 19:07:35 -0500

    6-Running Tests, finished

--- UltDjangoCourse/Part3/storefront3/store/tests/test_collections.py
+++ UltDjangoCourse/Part3/storefront3/store/tests/test_collections.py
@@ -1,9 +1,19 @@
 from rest_framework.test import APIClient
+from rest_framework import status
+import pytest
 
+# Ran into an issue involving pytest, installed pytest-django.(fixed)
+# pytest-django introduces additional decorators.
+ 
+@pytest.mark.django_db
 class TestCreateCollection:
 # test_ <-naming convention for pytest
     def test_if_user_is_anonymous_returns_401(self):
         # Arrange
         
         # Act
-        return
+        client = APIClient()
+        response = client.post('/store/collections/', {'title': 'a'})
+        
+        # Assert
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
