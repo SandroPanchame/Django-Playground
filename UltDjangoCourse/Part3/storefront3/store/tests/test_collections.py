d1429587b10e07bcb4229e758e99ca2214730138
SandroPanchame <spanchame@gmail.com>
2024-05-01 12:57:49 -0500

    11-Single or Multiple Assertions, finished

--- UltDjangoCourse/Part3/storefront3/store/tests/test_collections.py
+++ UltDjangoCourse/Part3/storefront3/store/tests/test_collections.py
@@ -1,4 +1,5 @@
 from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 from rest_framework import status
 import pytest
 
@@ -20,3 +21,28 @@ class TestCreateCollection:
         
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_if_data_is_invalid_returns_400(self):
+        client = APIClient()
+        # don't need to query the database, just create the user object
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': ''})
+        # best practice: tests should have 1 responsibility
+        # this test has two assertions because they are related 
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+    
+           
+    def test_if_data_is_valid_returns_201(self):
+        client = APIClient()
+
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/', {'title': 'a'})
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
